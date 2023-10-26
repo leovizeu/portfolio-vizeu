3e325594eaf3e8615e9981e4ce37f116e5f84794
Vizeu <leovizeu@hotmail.com>
2023-10-26 08:33:51 -0300

    Understanding component structure and basics

---
+++ src/app/Components/Menu.tsx
@@ -0,0 +1,12 @@
+function Menu() {
+    return (
+        <div>
+            <a href="#Home">Home</a>
+            <a href="#Projects">Projects</a>
+            <a href="#About me">About me</a>
+            <a href="#Contact">Contact</a>
+        </div>
+    )
+}
+
+export  default Menu
