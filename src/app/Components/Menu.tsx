8cbbf59188c284a6baf73183097fe36f455eaf1e
Vizeu <leovizeu@hotmail.com>
2023-12-23 01:23:09 -0300

    started training messing around

--- src/app/Components/Menu.tsx
+++ src/app/Components/Menu.tsx
@@ -1,11 +1,16 @@
 function Menu() {
     return (
-        <div className="flex flex-col items-center justify-between p-24">
-            <a href="#Home">Home</a>
-            <a href="#Projects">Projects</a>
-            <a href="#About me">About me</a>
-            <a href="#Contact">Contact</a>
-        </div>
+        <div className='z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex'>
+            <div className='fixed left-0 top-0 flex w-full justify-evenly'>
+                <ul>
+                    <li><p>Portfolio</p></li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About me</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+      </div>
     )
 }
 
