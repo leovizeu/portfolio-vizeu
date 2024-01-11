98ff8afb12eae15cd923e002a39817342b6368d1
Vizeu <leovizeu@hotmail.com>
2024-01-11 00:19:34 -0300

    styling some button

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -14,12 +14,13 @@ function Hero() {
                     Greetings! <br />
                     I’m Full Stack Developer hoping to change carrers and start this new journey as a software developer. Here's what i do:
                 </p>
-
-                <CustomButtom 
-                    title ="Contact me"
-                    containerStyles = "bg-blue-500 text-white rounded-full p-2"
-                    handleClick = {handleScroll}
-                />
+                <div className="flex flex-row-reverse pr-5 pb-5">
+                    <CustomButtom
+                        title ="Contact me"
+                        containerStyles = "bg-blue-500 text-white font-bold rounded-full p-2 hover:bg-black hover:text-white"
+                        handleClick = {handleScroll}
+                    />
+                </div>
         </div>
     )
 }
