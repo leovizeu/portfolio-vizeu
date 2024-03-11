4292df92fd392939a6d93f1629e04cc4ecfe28b1
Vizeu <leovizeu@hotmail.com>
2024-03-11 14:07:25 -0300

    add glare to the parallax cards

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -8,8 +8,8 @@ import Modal from './ModalRoute';
 
 function Card ({title, title2, handleClick, imgSrc, cardAlt, live, code}: CardProps) {
   return (
-    <div className="h-full w-80 relative overflow-hidden flex flex-col justify-center items-center">    
-      <Tilt>
+    <div className="relative w-80 h-96 gap-3 overflow-hidden flex flex-col justify-center items-center">    
+      <Tilt glareEnable glareMaxOpacity={10} glareColor='white' glarePosition='bottom' glareBorderRadius='1rem'>
         <div className="flex justify-center container h-60 w-60 bg-white bg-opacity-10 rounded-2xl shadow-5xl 
         relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm mb-3">
           <button
@@ -30,8 +30,7 @@ function Card ({title, title2, handleClick, imgSrc, cardAlt, live, code}: CardPr
           <a className='w-40 h-8 absolute' href={code} target='about_blank'></a>
           {title2}
         </span>
-      </div>
-              
+      </div> 
     </div>
   );
 }
